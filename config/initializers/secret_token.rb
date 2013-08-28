7ab771921d7c26dc8c70932d1dd1f80e1c9f3c38
Promit Biswas <promitbiswas@gmail.com>
2013-08-28 12:45:01 +0530

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Glug::Application.config.secret_token = 'd816e049b3f8fd91547b4569f0d376c3229a0647bc7cc3615f6f92af4571f4fc542a752812ac44c7315e46cf70211f95ce2ca069158539e9769a9db3d8c995ce'
